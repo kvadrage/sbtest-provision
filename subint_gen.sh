e530767f76b873b6bdb5b8728fe9cbeabd118c38
kvadrage <kvadrage@gmail.com>
2018-04-26 14:51:14 +0300

    major fixes

--- subint_gen.sh
+++ subint_gen.sh
@@ -14,38 +14,38 @@ else
 fi
 
 echo "Generating 256 vlan interfaces for $1 (3rd octet = $ref+)"
-for i in `seq 2 4 256`;
+for i in `seq 1 2 256`;
 do
-    let j=i+0-1
+    let j=i
     let sub=ref+1
     ip link add link $1 name $1.$j type vlan id $j
     ip link set dev $1.$j up
-    ip addr add 10.252.$sub.$i/30 dev $1.$j
+    ip addr add 10.252.$sub.$i/31 dev $1.$j
 done
 
-for i in `seq 2 4 256`;
+for i in `seq 1 2 256`;
 do
-    let j=i+256-1
+    let j=i+256
     let sub=ref+2
     ip link add link $1 name $1.$j type vlan id $j
     ip link set dev $1.$j up
-    ip addr add 10.252.$sub.$i/30 dev $1.$j
+    ip addr add 10.252.$sub.$i/31 dev $1.$j
 done
 
-for i in `seq 2 4 256`;
+for i in `seq 1 2 256`;
 do
-    let j=i+512-1
+    let j=i+512
     let sub=ref+3
     ip link add link $1 name $1.$j type vlan id $j
     ip link set dev $1.$j up
-    ip addr add 10.252.$sub.$i/30 dev $1.$j
+    ip addr add 10.252.$sub.$i/31 dev $1.$j
 done
 
-for i in `seq 2 4 256`;
+for i in `seq 1 2 256`;
 do
-    let j=i+768-1
+    let j=i+768
     let sub=ref+4
     ip link add link $1 name $1.$j type vlan id $j
     ip link set dev $1.$j up
-    ip addr add 10.252.$sub.$i/30 dev $1.$j
+    ip addr add 10.252.$sub.$i/31 dev $1.$j
 done
